9e4e7683b54734fce638d4c19d1cffb31353f48d
Prashant Singh Bohra <95184941+PrashantSinghBohra@users.noreply.github.com>
2022-01-21 19:52:00 +0530

    Syntax Changed

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello{
   
  public static void main(String [] args)
  {
-  System.out.println("Hi,This code is for testing"); 
+  System.out.println("Hi,This code is for testing"
  }
 }
